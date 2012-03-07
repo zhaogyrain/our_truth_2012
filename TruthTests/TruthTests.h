f2ef513a56dcd97d6e2ea55288f85ce7502b07ae
zhaogyrain <zcl369369@163.com>
2012-03-07 14:18:32 +0800

    update README

---
+++ TruthTests/TruthTests.h
@@ -0,0 +1,13 @@
+//
+//  TruthTests.h
+//  TruthTests
+//
+//  Created by guangyu zhao on 12-2-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TruthTests : SenTestCase
+
+@end
---
+++ TruthTests/TruthTests.m
@@ -0,0 +1,32 @@
+//
+//  TruthTests.m
+//  TruthTests
+//
+//  Created by guangyu zhao on 12-2-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "TruthTests.h"
+
+@implementation TruthTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TruthTests");
+}
+
+@end
